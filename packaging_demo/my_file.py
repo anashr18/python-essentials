fdd836d605b0abf9783c685d2cd41e91d1c93d36
anashr18 <anand.shri18@gmail.com>
2024-07-10 16:22:33 +0530

    minor changes in cfg file

--- packaging_demo/my_file.py
+++ packaging_demo/my_file.py
@@ -1,11 +1,7 @@
 import numpy as np
 
-# from my_folder.my_nested_file import CONSTANT as CONSTANT2
 from packaging_demo.my_folder.my_nested_file import CONSTANT as CONSTANT2
-
-# from my_other_file import CONSTANT
 
 ARRAY = np.array([1, 2, 4])
 print(ARRAY)
-# print(CONSTANT)
 print(CONSTANT2)
